55f888842dcb93f092302b3757b73f15ed9e903c
J. Freiheit <freiheit@htw-berlin.de>
2022-04-26 17:35:20 +0200

    Test nach Caching

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,24 @@
 self.addEventListener('install', event => {
     console.log('service worker --> installing ...', event);
+    event.waitUntil(
+        caches.open('static')
+            .then( cache => {
+                console.log('Service-Worker-Cache erzeugt und offen');
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/js/material.min.js',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/images/htw.jpg',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://code.getmdl.io/1.3.0/material.blue_grey-red.min.css'
+                ]);
+            })
+    );
 })
 
 self.addEventListener('activate', event => {
@@ -8,5 +27,20 @@ self.addEventListener('activate', event => {
 })
 
 self.addEventListener('fetch', event => {
-    event.respondWith(fetch(event.request));
+        event.respondWith(
+        caches.match(event.request)
+            .then( response => {
+                if(response) {
+                    return response;
+                } else {
+                    return fetch(event.request)
+                    .then( res => {             // nicht erneut response nehmen, haben wir schon
+                        caches.open('dynamic')      // neuer, weiterer Cache namens dynamic
+                            .then( cache => {
+                                cache.put(event.request.url, res);      // hier die put-Anweisung
+                            })
+                    });
+                }
+            })
+    );
 })
